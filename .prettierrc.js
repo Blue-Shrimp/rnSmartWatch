7867f6049e8050f0e3b717456fdb39ab6faad938
shjang <shj@logsynk.com>
2023-10-10 13:09:02 +0900

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 150,
+  semi: false,
+  endOfLine: 'lf',
+  tabWidth: 2,
+  useTabs: false,
+};
